924353f08f0d130c3156a1f7d3c77a4aa3239f54
krishbasra <krish.basra@noseryoung.ch>
2022-11-15 09:49:52 +0100

    init

--- K01/Content/K01_KicksApp/src/ch/noseryoung/blj/Main.java
+++ K01/Content/K01_KicksApp/src/ch/noseryoung/blj/Main.java
@@ -8,7 +8,7 @@ public class Main {
 private static boolean isRunning = true;
 
     public static void main(String[] args) {
-        boolean cont = true;
+        int antwort;
         do {
             System.out.println("+-----------------------------------+");
             System.out.println("|\t[1]  - Football Teams           |");
@@ -20,18 +20,13 @@ private static boolean isRunning = true;
                 printTeams();
             } else if (choiceentry == 2) {
                 printPlayers();
-            }else{
+            } else {
                 System.out.println("Invalid Input");
             }
             System.out.println("----------------------------------------------");
             System.out.println("Wollen Sie weitermachen  1-ja  0-nein");
-            int antwort = scanchoice.nextInt();
-            if (antwort == 1) {
-                cont = true;
-            } else {
-                cont = false;
-            }
-        } while (cont);
+             antwort = scanchoice.nextInt();
+        } while (antwort == 1);
     }
 
 
